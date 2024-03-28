685e74415ac4111f41122e10356940613d372cd6
Carlos André Batalha Pontes <carlos.pontes@itriad.org.br>
2024-03-28 11:49:15 -0400

    [dv-12] create contato component

---
+++ src/app/institucional/contato/contato.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contato',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      contato works!
+    </p>
+  `,
+  styles: ``
+})
+export class ContatoComponent {
+
+}

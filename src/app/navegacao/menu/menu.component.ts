76d07f8c866fd33136e9d288dede19d0d3751c31
Carlos André Batalha Pontes <carlos.pontes@itriad.org.br>
2024-03-28 11:20:31 -0400

    [dv-09] create menu component

---
+++ src/app/navegacao/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './menu.component.html'
+})
+export class MenuComponent {
+
+}

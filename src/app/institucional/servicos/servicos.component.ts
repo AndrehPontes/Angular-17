1aa715123cb1cc30231e69dbde9c585509686886
Carlos André Batalha Pontes <carlos.pontes@itriad.org.br>
2024-03-28 11:20:11 -0400

    [dv-08] create services component

---
+++ src/app/institucional/servicos/servicos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicos',
+  standalone: true,
+  imports: [],
+  templateUrl: './servicos.component.html'
+})
+export class ServicosComponent {
+
+}

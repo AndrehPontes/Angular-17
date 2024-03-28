aa050b766bf4022f04e661b473bb6ba83e9ab008
Carlos André Batalha Pontes <carlos.pontes@itriad.org.br>
2024-03-28 11:18:35 -0400

    [dv-07] create routes defualt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from "./navegacao/menu/menu.component";
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ServicosComponent } from './institucional/servicos/servicos.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: true
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: true,
+    imports: [MenuComponent, FooterComponent, HomeComponent, ServicosComponent, RouterOutlet, RouterModule]
 })
 export class AppComponent {
   title = 'MyProject';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './navegacao/home/home.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ServicosComponent } from './institucional/servicos/servicos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'/home', pathMatch: 'full'},
+    {path:'home', component: HomeComponent},    
+    {path:'sobre', component: SobreComponent},
+    {path:'servicos', component: ServicosComponent},
+    {path:'contato', component: ContatoComponent},
+];

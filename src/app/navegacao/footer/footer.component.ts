c0a575dc0dd17922a25027ea5e9f4ac9017d7ff9
Carlos André Batalha Pontes <carlos.pontes@itriad.org.br>
2024-03-28 11:21:13 -0400

    [dv-11] create footer component

---
+++ src/app/navegacao/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../../app.component';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html'
+})
+export class FooterComponent {
+
+}

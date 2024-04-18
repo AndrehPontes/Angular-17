04a0c60eb857b075882ca3e99ab89dc9bf3fbb4f
Carlos André Batalha Pontes <carlos.pontes@itriad.org.br>
2024-04-18 00:30:24 -0400

    [dv-17] two-way binding

--- src/app/demos/data-binding/data-binding.component.ts
+++ src/app/demos/data-binding/data-binding.component.ts
@@ -1,14 +1,17 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,NgClass],
   templateUrl: './data-binding.component.html'
 })
 export class DataBindingComponent {
   public contadorClique: number = 0;
   public urlImagem: string = "https://angular.io/assets/images/logos/angular/logo-nav@2x.png"
+  public nome: string = ''
 
   adicionarClique(){
     this.contadorClique ++;

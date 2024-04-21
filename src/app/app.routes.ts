bef7cfa40d0546fd4bc166305855fac96792fdb6
Carlos André Batalha Pontes <carlos.pontes@itriad.org.br>
2024-04-21 17:34:36 -0400

    [dv-21] create product component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ContatoComponent } from './institucional/contato/contato.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ServicosComponent } from './institucional/servicos/servicos.component';
 import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'/home', pathMatch: 'full'},
@@ -12,4 +13,5 @@ export const routes: Routes = [
     {path:'servicos', component: ServicosComponent},
     {path:'contato', component: ContatoComponent},
     {path:'faeture-data-binding', component: DataBindingComponent},
+    {path:'produtos', component: ListaProdutoComponent},
 ];
